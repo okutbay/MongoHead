8bba2a311dc12dd1f78f1cfb6a9e0c46c50ab332
ozan.bayram <okutbay@gmail.com>
2018-03-01 16:09:02 +0300

    New sample console app added

---
+++ MongoHeadSampleApp/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace MongoHeadSampleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+
+            //In this sample App we need do use business functions
+            //So we neen an dependency on App Domain
+
+        }
+    }
+
+
+
+
+}
