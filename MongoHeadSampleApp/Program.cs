df7a7094451d1aae91236d324e8408335f20a74c
Ozan Bayram <okutbay@gmail.com>
2018-05-06 23:32:44 +0200

    .

--- MongoHeadSampleApp/Program.cs
+++ MongoHeadSampleApp/Program.cs
@@ -12,6 +12,12 @@ namespace MongoHeadSampleApp
             //In this sample App we need do use business functions
             //So we neen an dependency on App Domain
 
+
+
+
+
+            Console.ReadLine();
+
         }
     }
 
