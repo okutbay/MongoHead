7be1aa45f8d31578b1f8ba785c8371f21e63212e
ozan.bayram <okutbay@gmail.com>
2018-07-19 21:37:37 +0300

    Test related codes are added

--- MongoHeadSample/Controllers/HomeController.cs
+++ MongoHeadSample/Controllers/HomeController.cs
@@ -4,12 +4,21 @@ using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using MongoHead;
 using MongoHeadSample.Models;
 
 namespace MongoHeadSample.Controllers
 {
     public class HomeController : Controller
     {
+        private IConfiguration _configuration;
+
+        public HomeController(IConfiguration Configuration)
+        {
+            _configuration = Configuration;
+        }
+
         public IActionResult Index()
         {
             return View();
@@ -17,6 +26,31 @@ namespace MongoHeadSample.Controllers
 
         public IActionResult Test()
         {
+            //TODO BUNU BURADA YAPMAYALIM YAHU. GIDELIM KENDI ORNEK PROJESINDE YAPALIM
+
+
+            List<Filter> filter = new List<Filter>()
+            {
+                new Filter { PropertyName = "ShortenedURL", Operation = Op.Equals, Value = "http://cut.lu/c87sft" }
+            };
+
+            MongoDBConfig config = new MongoDBConfig(
+                _configuration["MongoDBConfig:ConnectionString"],
+                _configuration["MongoDBConfig:DefaultDatabaseName"]
+                );
+
+            MongoDBHelper helper = new MongoDBHelper(config, typeof(Test));
+
+            //Burada helper metodlarımızı bir test edelim
+
+
+            Test foundItem = helper.Get<Test>(filter);
+
+
+
+            //burada bir basedata metodlarımızı test edelim
+
+
             return View();
         }
 
---
+++ MongoHeadSample/Models/Test.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.Extensions.Configuration;
+using MongoHead;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MongoHeadSample.Models
+{
+    //BUSINESS LAYER
+    public class TestBusiness
+    {
+        readonly IConfiguration _configuration;
+
+        public TestBusiness(IConfiguration Configuration)
+        {
+            this._configuration = Configuration;
+        }
+
+        public List<Test> GetBySurname(string Surname)
+        {
+            List<Filter> filter = new List<Filter>()
+            {
+                new Filter { PropertyName = "Surname", Operation = Op.Equals, Value = Surname }
+            };
+
+            TestData testData = new TestData(_configuration);
+
+            List<Test> foundItem = testData.GetList(filter);
+
+            return foundItem;
+        }
+    }
+
+    //DATA LAYER
+    internal class TestData : MongoHead.BaseData<Test>
+    {
+        //TODO Which one is good using generic type T or explicit type like Test
+        //imho explicit type
+
+        readonly IConfiguration _configuration;
+
+        string CollectionName { get; set; }
+
+        public TestData(IConfiguration Configuration) : base(Configuration)
+        {
+            this._configuration = Configuration;
+            this.CollectionName = typeof(Test).Name;
+        }
+    }
+
+    //ENTITY DEFINITION
+    public class Test : BaseEntity
+    {
+        public Test() : base()
+        { }
+
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public bool AliveAndKicking { get; set; }
+    }
+
+
+
+
+}
