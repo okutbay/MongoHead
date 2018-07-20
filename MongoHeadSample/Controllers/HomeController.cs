f5551aa7cf5940d2317f6690b551743fa5058ff7
ozan.bayram <okutbay@gmail.com>
2018-07-20 17:52:43 +0300

    Save & GetList helper method samples are added

--- MongoHeadSample/Controllers/HomeController.cs
+++ MongoHeadSample/Controllers/HomeController.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
+using MongoDB.Bson;
 using MongoHead;
 using MongoHeadSample.Models;
 
@@ -26,29 +27,65 @@ namespace MongoHeadSample.Controllers
 
         public IActionResult Test()
         {
-            //TODO BUNU BURADA YAPMAYALIM YAHU. GIDELIM KENDI ORNEK PROJESINDE YAPALIM
 
 
-            List<Filter> filter = new List<Filter>()
-            {
-                new Filter { PropertyName = "ShortenedURL", Operation = Op.Equals, Value = "http://cut.lu/c87sft" }
-            };
 
             MongoDBConfig config = new MongoDBConfig(
-                _configuration["MongoDBConfig:ConnectionString"],
-                _configuration["MongoDBConfig:DefaultDatabaseName"]
+                _configuration[MongoDBConfig.KeyNameConnectionString],
+                _configuration[MongoDBConfig.KeyNameDatabaseName]
                 );
 
             MongoDBHelper helper = new MongoDBHelper(config, typeof(Test));
 
-            //Burada helper metodlarımızı bir test edelim
+            Test test = new Test();
+            test.AliveAndKicking = true;
+            test.DateOfBirth = new DateTime(1976, 4, 19);
+            test.Name = "Ozan Kutlu";
+            test.Surname = "Bayram";
+            test._DateUtcCreated = DateTime.Now;
+            test._DateUtcModified = DateTime.Now;
+            test._IsActive = true;
+
+            //MongoDBHelper method samples for Test entity
+
+            //save object to db
+            test._id = helper.Save(test);
 
 
-            Test foundItem = helper.Get<Test>(filter);
+            //save some json
+            //Please note that at least one field other than id must match to a entity property to get deserialized properly while retreiving from DB. 
+            //Otherwise you will get format exception when you try to get that document.
+            string json = "{ 'Name' : 'Hakkı' }";
+            MongoDB.Bson.BsonDocument someBsonDocument
+                = MongoDB.Bson.Serialization.BsonSerializer.Deserialize<BsonDocument>(json);
+            ObjectId fooId = helper.Save(someBsonDocument);
+
+
+            //GetList<T>
+            List<Test> foundItems1 = helper.GetList<Test>();
+
+
+            //Get List by filter
+            List<Filter> filterByName1 = new List<Filter>()
+            {
+                new Filter { PropertyName = "Name", Operation = Op.Equals, Value = "Hakkı" }
+            };
+            List<Test> foundItems2 = helper.GetList<Test>(filterByName1);
+
+
+            //get item by filter
+            List<Filter> filterByName2 = new List<Filter>()
+            {
+                new Filter { PropertyName = "Name", Operation = Op.Equals, Value = "Ozan Kutlu" }
+            };
+            Test foundItem1 = helper.Get<Test>(filterByName2);
 
 
 
-            //burada bir basedata metodlarımızı test edelim
+            //BaseData method samples for Test entity
+
+
+
 
 
             return View();
