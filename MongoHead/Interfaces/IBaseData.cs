a6a0524d180c91b7a6b4c1baf6595f14668c28e1
ozan.bayram <okutbay@gmail.com>
2018-07-24 15:18:46 +0300

    - IBaseData method signatures refactored

--- MongoHead/Interfaces/IBaseData.cs
+++ MongoHead/Interfaces/IBaseData.cs
@@ -9,9 +9,9 @@ namespace MongoHead.Interfaces
     public interface IBaseData<T>
     {
         List<T> GetList();
-        List<T> GetList(List<Filter> filter, bool UseAndLogic = true);
-        Dictionary<string, string> GetKeyValueList(List<Filter> filter, bool UseAndLogic = true);
-        Dictionary<string, string> GetKeyValueList(string KeyFieldName, string ValueFieldName, List<Filter> filter, bool UseAndLogic = true);
+        List<T> GetList(List<Filter> Filter, bool UseAndLogic = true);
+        Dictionary<string, string> GetKeyValueList(List<Filter> Filter, bool UseAndLogic = true);
+        Dictionary<string, string> GetKeyValueList(string KeyFieldName, string ValueFieldName, List<Filter> Filter, bool UseAndLogic = true);
 
         T GetById(string Id);
         T GetById(ObjectId Id);
